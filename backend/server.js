e5bac19ea99e09d7469476055f348846f613afdc
adrymendez <adryleandromendez.b@gmail.com>
2026-05-07 16:27:46 -0400

    Backend node funcionando

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Backend Barber App funcionando');
+});
+
+app.get('/api/test', (req, res) => {
+    res.json({
+        ok: true,
+        message: 'API funcionando'
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
