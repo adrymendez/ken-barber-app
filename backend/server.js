20ac59856a268a4327edbc50d89f5ac4b6b10027
adrymendez <adryleandromendez.b@gmail.com>
2026-05-08 15:21:21 -0400

    Backend funcionando local

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Backend Barber App funcionando');
+    res.send('Servidor funcionando');
 });
 
 app.get('/api/test', (req, res) => {
@@ -19,7 +19,7 @@ app.get('/api/test', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
